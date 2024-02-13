f44bb837facfca37350baa5f451e4f3387ce385f
nero <devstory.co.kr@gmail.com>
2024-02-13 13:42:57 +0900

    Rename arb-translator to flutter-translator.

--- src/app/util/constant.ts
+++ src/app/util/constant.ts
@@ -1,5 +1,5 @@
 export class Constant {
   public static appName = "flutter-translator";
   public static homePage =
-    "https://marketplace.visualstudio.com/items?itemName=DevStory.arb-translator";
+    "https://marketplace.visualstudio.com/items?itemName=DevStory.flutter-translator";
 }

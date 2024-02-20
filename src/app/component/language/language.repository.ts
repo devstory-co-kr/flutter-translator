9b198ca112f923982194c29edc2e6ad5ce401bc4
nero <devstory.co.kr@gmail.com>
2024-02-20 14:35:27 +0900

    Add isLTR property to language.

--- src/app/component/language/language.repository.ts
+++ src/app/component/language/language.repository.ts
@@ -2,638 +2,803 @@ import { Language } from "./language";
 
 export class LanguageRepository {
   public static afrikaans: Language = {
+    isLTR: true,
     gt: "af",
     name: "Afrikaans",
     languageCode: "af",
   };
   public static albanian: Language = {
+    isLTR: true,
     gt: "sq",
     name: "Albanian",
     languageCode: "sq",
   };
   public static amharic: Language = {
+    isLTR: false,
     gt: "am",
     name: "Amharic",
     languageCode: "am",
   };
   public static arabic: Language = {
+    isLTR: true,
     gt: "ar",
     name: "Arabic",
     languageCode: "ar",
   };
   public static armenian: Language = {
+    isLTR: true,
     gt: "hy",
     name: "Armenian",
     languageCode: "hy",
   };
   public static assamese: Language = {
+    isLTR: true,
     gt: "as",
     name: "Assamese",
     languageCode: "as",
   };
   public static aymara: Language = {
+    isLTR: true,
     gt: "ay",
     name: "Aymara",
     languageCode: "ay",
   };
   public static azerbaijani: Language = {
+    isLTR: true,
     gt: "az",
     name: "Azerbaijani",
     languageCode: "az",
   };
   public static bambara: Language = {
+    isLTR: true,
     gt: "bm",
     name: "Bambara",
     languageCode: "bm",
   };
   public static basque: Language = {
+    isLTR: true,
     gt: "eu",
     name: "Basque",
     languageCode: "eu",
   };
   public static belarusian: Language = {
+    isLTR: true,
     gt: "be",
     name: "Belarusian",
     languageCode: "be",
   };
   public static bengali: Language = {
+    isLTR: true,
     gt: "bn",
     name: "Bengali",
     languageCode: "bn",
   };
   public static bhojpuri: Language = {
+    isLTR: true,
     gt: "bho",
     name: "Bhojpuri",
     languageCode: "bho",
   };
   public static bosnian: Language = {
+    isLTR: true,
     gt: "bs",
     name: "Bosnian",
     languageCode: "bs",
   };
   public static bulgarian: Language = {
+    isLTR: true,
     gt: "bg",
     name: "Bulgarian",
     languageCode: "bg",
   };
   public static catalan: Language = {
+    isLTR: true,
     gt: "ca",
     name: "Catalan",
     languageCode: "ca",
   };
   public static cebuano: Language = {
+    isLTR: true,
     gt: "ceb",
     name: "Cebuano",
     languageCode: "ceb",
   };
   public static chineseSimplified: Language = {
+    isLTR: true,
     gt: "zh-CN",
     name: "Chinese Simplified",
     languageCode: "zh_CN",
   };
   public static chineseTraditional: Language = {
+    isLTR: true,
     gt: "zh-TW",
     name: "Chinese Traditional",
     languageCode: "zh_TW",
   };
   public static corsican: Language = {
+    isLTR: true,
     gt: "co",
     name: "Corsican",
     languageCode: "co",
   };
   public static croatian: Language = {
+    isLTR: true,
     gt: "hr",
     name: "Croatian",
     languageCode: "hr",
   };
   public static czech: Language = {
+    isLTR: true,
     gt: "cs",
     name: "Czech",
     languageCode: "cs",
   };
   public static danish: Language = {
+    isLTR: true,
     gt: "da",
     name: "Danish",
     languageCode: "da",
   };
   public static dhivehi: Language = {
+    isLTR: true,
     gt: "dv",
     name: "Dhivehi",
     languageCode: "dv",
   };
   public static dogri: Language = {
+    isLTR: true,
     gt: "doi",
     name: "Dogri",
     languageCode: "doi",
   };
   public static dutch: Language = {
+    isLTR: true,
     gt: "nl",
     name: "Dutch",
     languageCode: "nl",
   };
   public static english: Language = {
+    isLTR: true,
     gt: "en",
     name: "English",
     languageCode: "en",
   };
   public static esperanto: Language = {
+    isLTR: true,
     gt: "eo",
     name: "Esperanto",
     languageCode: "eo",
   };
   public static estonian: Language = {
+    isLTR: true,
     gt: "et",
     name: "Estonian",
     languageCode: "et",
   };
-  public static ewe: Language = { gt: "ee", name: "Ewe", languageCode: "ee" };
+  public static ewe: Language = {
+    isLTR: true,
+    gt: "ee",
+    name: "Ewe",
+    languageCode: "ee",
+  };
   public static finnish: Language = {
+    isLTR: true,
     gt: "fi",
     name: "Finnish",
     languageCode: "fi",
   };
   public static french: Language = {
+    isLTR: true,
     gt: "fr",
     name: "French",
     languageCode: "fr",
   };
   public static frisian: Language = {
+    isLTR: true,
     gt: "fy",
     name: "Frisian",
     languageCode: "fy",
   };
   public static galician: Language = {
+    isLTR: true,
     gt: "gl",
     name: "Galician",
     languageCode: "gl",
   };
   public static georgian: Language = {
+    isLTR: true,
     gt: "ka",
     name: "Georgian",
     languageCode: "ka",
   };
   public static german: Language = {
+    isLTR: true,
     gt: "de",
     name: "German",
     languageCode: "de",
   };
   public static greek: Language = {
+    isLTR: true,
     gt: "el",
     name: "Greek",
     languageCode: "el",
   };
   public static guarani: Language = {
+    isLTR: true,
     gt: "gn",
     name: "Guarani",
     languageCode: "gn",
   };
   public static gujarati: Language = {
+    isLTR: true,
     gt: "gu",
     name: "Gujarati",
     languageCode: "gu",
   };
   public static haitianCreole: Language = {
+    isLTR: true,
     gt: "ht",
     name: "Haitian Creole",
     languageCode: "ht",
   };
   public static hausa: Language = {
+    isLTR: true,
     gt: "ha",
     name: "Hausa",
     languageCode: "ha",
   };
   public static hawaiian: Language = {
+    isLTR: true,
     gt: "haw",
     name: "Hawaiian",
     languageCode: "haw",
   };
   public static hebrew: Language = {
+    isLTR: false,
     gt: "iw",
     name: "Hebrew",
     languageCode: "he",
   };
   public static hindi: Language = {
+    isLTR: true,
     gt: "hi",
     name: "Hindi",
     languageCode: "hi",
   };
   public static hmong: Language = {
+    isLTR: true,
     gt: "hmn",
     name: "Hmong",
     languageCode: "hmn",
   };
   public static hungarian: Language = {
+    isLTR: true,
     gt: "hu",
     name: "Hungarian",
     languageCode: "hu",
   };
   public static icelandic: Language = {
+    isLTR: true,
     gt: "is",
     name: "Icelandic",
     languageCode: "is",
   };
-  public static igbo: Language = { gt: "ig", name: "Igbo", languageCode: "ig" };
+  public static igbo: Language = {
+    isLTR: true,
+    gt: "ig",
+    name: "Igbo",
+    languageCode: "ig",
+  };
   public static ilocano: Language = {
+    isLTR: true,
     gt: "ilo",
     name: "Ilocano",
     languageCode: "ilo",
   };
   public static indonesian: Language = {
+    isLTR: true,
     gt: "id",
     name: "Indonesian",
     languageCode: "id",
   };
   public static irish: Language = {
+    isLTR: true,
     gt: "ga",
     name: "Irish",
     languageCode: "ga",
   };
   public static italian: Language = {
+    isLTR: true,
     gt: "it",
     name: "Italian",
     languageCode: "it",
   };
   public static japanese: Language = {
+    isLTR: true,
     gt: "ja",
     name: "Japanese",
     languageCode: "ja",
   };
   public static javanese: Language = {
+    isLTR: true,
     gt: "jw",
     name: "Javanese",
     languageCode: "jw",
   };
   public static kannada: Language = {
+    isLTR: true,
     gt: "kn",
     name: "Kannada",
     languageCode: "kn",
   };
   public static kazakh: Language = {
+    isLTR: true,
     gt: "kk",
     name: "Kazakh",
     languageCode: "kk",
   };
   public static khmer: Language = {
+    isLTR: true,
     gt: "km",
     name: "Khmer",
     languageCode: "km",
   };
   public static kinyarwanda: Language = {
+    isLTR: true,
     gt: "rw",
     name: "Kinyarwanda",
     languageCode: "rw",
   };
   public static konkani: Language = {
+    isLTR: true,
     gt: "gom",
     name: "Konkani",
     languageCode: "gom",
   };
   public static korean: Language = {
+    isLTR: true,
     gt: "ko",
     name: "Korean",
     languageCode: "ko",
   };
   public static krio: Language = {
+    isLTR: true,
     gt: "kri",
     name: "Krio",
     languageCode: "kri",
   };
   public static kurdish: Language = {
+    isLTR: true,
     gt: "ku",
     name: "Kurdish",
     languageCode: "ku",
   };
   public static kurdishSorani: Language = {
+    isLTR: true,
     gt: "ckb",
     name: "Kurdish Sorani",
     languageCode: "ckb",
   };
   public static kyrgyz: Language = {
+    isLTR: true,
     gt: "ky",
     name: "Kyrgyz",
     languageCode: "ky",
   };
-  public static lao: Language = { gt: "lo", name: "Lao", languageCode: "lo" };
+  public static lao: Language = {
+    isLTR: true,
+    gt: "lo",
+    name: "Lao",
+    languageCode: "lo",
+  };
   public static latin: Language = {
+    isLTR: true,
     gt: "la",
     name: "Latin",
     languageCode: "la",
   };
   public static latvian: Language = {
+    isLTR: true,
     gt: "lv",
     name: "Latvian",
     languageCode: "lv",
   };
   public static lingala: Language = {
+    isLTR: true,
     gt: "ln",
     name: "Lingala",
     languageCode: "ln",
   };
   public static lithuanian: Language = {
+    isLTR: true,
     gt: "lt",
     name: "Lithuanian",
     languageCode: "lt",
   };
   public static luganda: Language = {
+    isLTR: true,
     gt: "lg",
     name: "Luganda",
     languageCode: "lg",
   };
   public static luxembourgish: Language = {
+    isLTR: true,
     gt: "lb",
     name: "Luxembourgish",
     languageCode: "lb",
   };
   public static macedonian: Language = {
+    isLTR: true,
     gt: "mk",
     name: "Macedonian",
     languageCode: "mk",
   };
   public static maithili: Language = {
+    isLTR: true,
     gt: "mai",
     name: "Maithili",
     languageCode: "mai",
   };
   public static malagasy: Language = {
+    isLTR: true,
     gt: "mg",
     name: "Malagasy",
     languageCode: "mg",
   };
   public static malay: Language = {
+    isLTR: true,
     gt: "ms",
     name: "Malay",
     languageCode: "ms",
   };
   public static malayalam: Language = {
+    isLTR: true,
     gt: "ml",
     name: "Malayalam",
     languageCode: "ml",
   };
   public static maltese: Language = {
+    isLTR: true,
     gt: "mt",
     name: "Maltese",
     languageCode: "mt",
   };
   public static maori: Language = {
+    isLTR: true,
     gt: "mi",
     name: "Maori",
     languageCode: "mi",
   };
   public static marathi: Language = {
+    isLTR: true,
     gt: "mr",
     name: "Marathi",
     languageCode: "mr",
   };
   public static meiteilon: Language = {
+    isLTR: true,
     gt: "mni-Mtei",
     name: "Meiteilon",
     languageCode: "mni",
   };
   public static mizo: Language = {
+    isLTR: true,
     gt: "lus",
     name: "Mizo",
     languageCode: "lus",
   };
   public static mongolian: Language = {
+    isLTR: true,
     gt: "mn",
     name: "Mongolian",
     languageCode: "mn",
   };
   public static myanmar: Language = {
+    isLTR: true,
     gt: "my",
     name: "Myanmar",
     languageCode: "my",
   };
   public static nepali: Language = {
+    isLTR: true,
     gt: "ne",
     name: "Nepali",
     languageCode: "ne",
   };
   public static norwegian: Language = {
+    isLTR: true,
     gt: "no",
     name: "Norwegian",
     languageCode: "no",
   };
   public static nyanja: Language = {
+    isLTR: true,
     gt: "ny",
     name: "Nyanja",
     languageCode: "ny",
   };
-  public static odia: Language = { gt: "or", name: "Odia", languageCode: "or" };
+  public static odia: Language = {
+    isLTR: true,
+    gt: "or",
+    name: "Odia",
+    languageCode: "or",
+  };
   public static oromo: Language = {
+    isLTR: true,
     gt: "om",
     name: "Oromo",
     languageCode: "om",
   };
   public static pashto: Language = {
+    isLTR: true,
     gt: "ps",
     name: "Pashto",
     languageCode: "ps",
   };
   public static persian: Language = {
+    isLTR: false,
     gt: "fa",
     name: "Persian",
     languageCode: "fa",
   };
   public static polish: Language = {
+    isLTR: true,
     gt: "pl",
     name: "Polish",
     languageCode: "pl",
   };
   public static portuguese: Language = {
+    isLTR: true,
     gt: "pt",
     name: "Portuguese",
     languageCode: "pt",
   };
   public static punjabi: Language = {
+    isLTR: true,
     gt: "pa",
     name: "Punjabi",
     languageCode: "pa",
   };
   public static quechua: Language = {
+    isLTR: true,
     gt: "qu",
     name: "Quechua",
     languageCode: "qu",
   };
   public static romanian: Language = {
+    isLTR: true,
     gt: "ro",
     name: "Romanian",
     languageCode: "ro",
   };
   public static russian: Language = {
+    isLTR: true,
     gt: "ru",
     name: "Russian",
     languageCode: "ru",
   };
   public static samoan: Language = {
+    isLTR: true,
     gt: "sm",
     name: "Samoan",
     languageCode: "sm",
   };
   public static sanskrit: Language = {
+    isLTR: true,
     gt: "sa",
     name: "Sanskrit",
     languageCode: "sa",
   };
   public static scotsGaelic: Language = {
+    isLTR: true,
     gt: "gd",
     name: "Scots Gaelic",
     languageCode: "gd",
   };
   public static sepedi: Language = {
+    isLTR: true,
     gt: "nso",
     name: "Sepedi",
     languageCode: "nso",
   };
   public static serbian: Language = {
+    isLTR: true,
     gt: "sr",
     name: "Serbian",
     languageCode: "sr",
   };
   public static sesotho: Language = {
+    isLTR: true,
     gt: "st",
     name: "Sesotho",
     languageCode: "st",
   };
   public static shona: Language = {
+    isLTR: true,
     gt: "sn",
     name: "Shona",
     languageCode: "sn",
   };
   public static sindhi: Language = {
+    isLTR: true,
     gt: "sd",
     name: "Sindhi",
     languageCode: "sd",
   };
   public static sinhala: Language = {
+    isLTR: true,
     gt: "si",
     name: "Sinhala",
     languageCode: "si",
   };
   public static slovak: Language = {
+    isLTR: true,
     gt: "sk",
     name: "Slovak",
     languageCode: "sk",
   };
   public static slovenian: Language = {
+    isLTR: true,
     gt: "sl",
     name: "Slovenian",
     languageCode: "sl",
   };
   public static somali: Language = {
+    isLTR: true,
     gt: "so",
     name: "Somali",
     languageCode: "so",
   };
   public static spanish: Language = {
+    isLTR: true,
     gt: "es",
     name: "Spanish",
     languageCode: "es",
   };
   public static sundanese: Language = {
+    isLTR: true,
     gt: "su",
     name: "Sundanese",
     languageCode: "su",
   };
   public static swahili: Language = {
+    isLTR: true,
     gt: "sw",
     name: "Swahili",
     languageCode: "sw",
   };
   public static swedish: Language = {
+    isLTR: true,
     gt: "sv",
     name: "Swedish",
     languageCode: "sv",
   };
   public static tagalog: Language = {
+    isLTR: true,
     gt: "tl",
     name: "Tagalog",
     languageCode: "tl",
   };
   public static tajik: Language = {
+    isLTR: true,
     gt: "tg",
     name: "Tajik",
     languageCode: "tg",
   };
   public static tamil: Language = {
+    isLTR: true,
     gt: "ta",
     name: "Tamil",
     languageCode: "ta",
   };
   public static tatar: Language = {
+    isLTR: true,
     gt: "tt",
     name: "Tatar",
     languageCode: "tt",
   };
   public static telugu: Language = {
+    isLTR: true,
     gt: "te",
     name: "Telugu",
     languageCode: "te",
   };
-  public static thai: Language = { gt: "th", name: "Thai", languageCode: "th" };
+  public static thai: Language = {
+    isLTR: true,
+    gt: "th",
+    name: "Thai",
+    languageCode: "th",
+  };
   public static tigrinya: Language = {
+    isLTR: true,
     gt: "ti",
     name: "Tigrinya",
     languageCode: "ti",
   };
   public static tsonga: Language = {
+    isLTR: true,
     gt: "ts",
     name: "Tsonga",
     languageCode: "ts",
   };
   public static turkish: Language = {
+    isLTR: true,
     gt: "tr",
     name: "Turkish",
     languageCode: "tr",
   };
   public static turkmen: Language = {
+    isLTR: true,
     gt: "tk",
     name: "Turkmen",
     languageCode: "tk",
   };
-  public static twi: Language = { gt: "ak", name: "Twi", languageCode: "ak" };
+  public static twi: Language = {
+    isLTR: true,
+    gt: "ak",
+    name: "Twi",
+    languageCode: "ak",
+  };
   public static ukrainian: Language = {
+    isLTR: true,
     gt: "uk",
     name: "Ukrainian",
     languageCode: "uk",
   };
-  public static urdu: Language = { gt: "ur", name: "Urdu", languageCode: "ur" };
+  public static urdu: Language = {
+    isLTR: false,
+    gt: "ur",
+    name: "Urdu",
+    languageCode: "ur",
+  };
   public static uyghur: Language = {
+    isLTR: true,
     gt: "ug",
     name: "Uyghur",
     languageCode: "ug",
   };
   public static uzbek: Language = {
+    isLTR: true,
     gt: "uz",
     name: "Uzbek",
     languageCode: "uz",
   };
   public static vietnamese: Language = {
+    isLTR: true,
     gt: "vi",
     name: "Vietnamese",
     languageCode: "vi",
   };
   public static welsh: Language = {
+    isLTR: true,
     gt: "cy",
     name: "Welsh",
     languageCode: "cy",
   };
   public static xhosa: Language = {
+    isLTR: true,
     gt: "xh",
     name: "Xhosa",
     languageCode: "xh",
   };
   public static yiddish: Language = {
+    isLTR: true,
     gt: "yi",
     name: "Yiddish",
     languageCode: "yi",
   };
   public static yoruba: Language = {
+    isLTR: true,
     gt: "yo",
     name: "Yoruba",
     languageCode: "yo",
   };
-  public static zulu: Language = { gt: "zu", name: "Zulu", languageCode: "zu" };
+  public static zulu: Language = {
+    isLTR: true,
+    gt: "zu",
+    name: "Zulu",
+    languageCode: "zu",
+  };
 
   public static supportLanguages: Language[] = [
     LanguageRepository.afrikaans,

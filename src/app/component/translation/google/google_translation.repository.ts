e8b527c07b324770a5524ae3f2ab8008f3f38cf0
nero <devstory.co.kr@gmail.com>
2024-03-12 21:37:51 +0900

    Change paramReplaceKeys to emoji due to an issue with breaking letters in Serbian.

--- src/app/component/translation/google/google_translation.repository.ts
+++ src/app/component/translation/google/google_translation.repository.ts
@@ -56,17 +56,16 @@ export class GoogleTranslationRepository implements TranslationRepository {
   }
 
   private paramReplaceKeys: string[] = [
-    "0️⃣",
-    "1️⃣",
-    "2️⃣",
-    "3️⃣",
-    "4️⃣",
-    "5️⃣",
-    "6️⃣",
-    "7️⃣",
-    "8️⃣",
-    "9️⃣",
-    "🔟",
+    "😀",
+    "😃",
+    "😄",
+    "😁",
+    "🥹",
+    "😅",
+    "😂",
+    "🤣",
+    "🥲",
+    "😊",
   ];
 
   /**
